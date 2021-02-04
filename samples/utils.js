a2d60dd3c17650f242f2a81dd50609774618eab4
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-02-04 10:37:53 +0200

    Update samples to use next versions

--- samples/utils.js
+++ samples/utils.js
@@ -1,10 +1,10 @@
 'use strict';
 
 (function(Utils) {
-	const chartjsUrl = 'https://cdn.jsdelivr.net/npm/chart.js@3.0.0-alpha.2/dist/chart.js';
+	const chartjsUrl = 'https://cdn.jsdelivr.net/npm/chart.js@next/dist/chart.js';
 	const dateFnsUrl = 'https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@1.1.0-alpha/dist/chartjs-adapter-date-fns.bundle.js';
 	const localUrl = '../dist/chartjs-chart-matrix.js';
-	const remoteUrl = 'https://cdn.jsdelivr.net/npm/chartjs-chart-matrix/dist/chartjs-chart-matrix.js';
+	const remoteUrl = 'https://cdn.jsdelivr.net/npm/chartjs-chart-matrix@next/dist/chartjs-chart-matrix.js';
 
 	function addScript(url, done, error) {
 		const head = document.getElementsByTagName('head')[0];
