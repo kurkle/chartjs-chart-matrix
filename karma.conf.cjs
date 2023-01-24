eb11e0c7726747e2a810b7052b8d4c129e464a02
Jukka Kurkela <jukka.kurkela@gmail.com>
2023-01-24 22:05:59 +0200

    fix tests, update deps, bump 2.0.1

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -5,7 +5,7 @@ const env = process.env.NODE_ENV;
 
 module.exports = async function(karma) {
   const builds = (await import('./rollup.config.js')).default;
-  const regex = karma.autoWatch ? /chartjs-chart-matrix\.js$/ : /chartjs-chart-matrix\.min\.js$/;
+  const regex = karma.autoWatch ? /chartjs-chart-matrix\.cjs$/ : /chartjs-chart-matrix\.min\.cjs$/;
   const build = builds.filter(v => v.output.file && v.output.file.match(regex))[0];
 
   if (env === 'test') {
