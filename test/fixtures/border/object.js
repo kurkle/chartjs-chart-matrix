2738d7af52dd1bf1953f9d3b6f0b5ffa83306cb7
Jukka Kurkela <jukka.kurkela@gmail.com>
2025-03-15 23:11:36 +0200

    test: add fixture for borders defined in object notation

---
+++ test/fixtures/border/object.js
@@ -0,0 +1,50 @@
+module.exports = {
+  config: {
+    type: 'matrix',
+    data: {
+      datasets: [
+        {
+          data: [
+            { x: 1, y: 1, v: 1 },
+            { x: 2, y: 1, v: 1 },
+            { x: 1, y: 2, v: 1 },
+            { x: 2, y: 2, v: 1 },
+          ],
+          backgroundColor: 'red',
+          borderColor: 'black',
+          borderWidth: [{ right: 10 }, { bottom: 10 }, { top: 10 }, { left: 10 }],
+          width: ({ chart }) => (chart.chartArea || {}).width / 2 - 1,
+          height: ({ chart }) => (chart.chartArea || {}).height / 2 - 1,
+        },
+      ],
+    },
+    options: {
+      events: [],
+      plugins: {
+        legend: false,
+      },
+      scales: {
+        x: {
+          type: 'linear',
+          display: false,
+          offset: false,
+          min: 0.5,
+          max: 2.5,
+        },
+        y: {
+          type: 'linear',
+          display: false,
+          offset: false,
+          min: 0.5,
+          max: 2.5,
+        },
+      },
+    },
+  },
+  options: {
+    canvas: {
+      height: 256,
+      width: 256,
+    },
+  },
+}
