ef691389a05a4a4e937ce9e95d57228f03b23452
Jukka Kurkela <jukka.kurkela@gmail.com>
2026-05-02 16:08:57 +0300

    chore(docs): upgrade astro, fix sitemap

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
+import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
 
 import { defineCollection, z } from 'astro:content'
 
 const docs = defineCollection({
+  loader: docsLoader(),
   schema: docsSchema({
     extend: z.object({}),
   }),
