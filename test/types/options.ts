66f17feacc21e35debe1c0aba162e5181504cbee
Jukka Kurkela <jukka.kurkela@gmail.com>
2026-05-11 19:47:08 +0300

    test: move type tests under test

--- test/types/options.ts
+++ test/types/options.ts
@@ -1,9 +1,22 @@
+import type { ChartConfiguration } from 'chart.js'
+import type { MatrixDataPoint, MatrixOptions } from 'chartjs-chart-matrix'
+
 import { Chart } from 'chart.js'
 import { MatrixController, MatrixElement } from 'chartjs-chart-matrix'
 
 Chart.register(MatrixController, MatrixElement)
 
-new Chart('test', {
+const matrixDataPoint: MatrixDataPoint = { v: 10, x: 1, y: 1 }
+
+const matrixOptions: Partial<MatrixOptions> = {
+  anchorX: 'center',
+  anchorY: 'top',
+  borderWidth: { bottom: 1, left: 2, right: 2, top: 1 },
+  height: 12,
+  width: 10,
+}
+
+const config: ChartConfiguration<'matrix', MatrixDataPoint[]> = {
   data: {
     datasets: [
       {
@@ -13,19 +26,20 @@ new Chart('test', {
           return context.dataIndex > -1 ? '#000' : '#fff'
         },
         borderWidth: 1,
-        data: [{ v: 10, x: 1, y: 1 }],
+        data: [matrixDataPoint, { v: 20, x: '2', y: '3' }],
         height(context) {
-          return context.dataIndex > -1 ? 10 : 0
+          return context.dataset.data[context.dataIndex]?.v === 20 ? 20 : 10
         },
         hoverBorderWidth: () => 2,
         label: 'Matrix',
         width(context) {
-          return context.dataIndex > -1 ? 10 : 0
+          return context.dataset.data[context.dataIndex]?.x === '2' ? 20 : 10
         },
       },
     ],
   },
   options: {
+    ...matrixOptions,
     plugins: {
       tooltip: {
         callbacks: {
@@ -40,7 +54,12 @@ new Chart('test', {
       x: {
         type: 'linear',
       },
+      y: {
+        type: 'category',
+      },
     },
   },
   type: 'matrix',
-})
+}
+
+new Chart('test', config)
